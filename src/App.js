c0e0d063dd3ebb7670aef27121d308dab0e31a92
DTheLegend <wampamba.daudi@gmail.com>
2022-03-22 03:17:41 +0000

    Fix indentation

--- src/App.js
+++ src/App.js
@@ -5,33 +5,31 @@ import QuestionPanel from './components/QuestionPanel';
 // https://metmuseum.github.io/
 
 function App({department}) {
-  let [objectIds, setObjectIds] = useState()
+    let [objectIds, setObjectIds] = useState()
 
-  useEffect(() => {
-      (async () => {
-          let response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${department}`);
-          
-          if(!response.ok) {
-              setObjectIds(null);
-              console.error(response);
-              return;
-          }
+    useEffect(() => {
+        (async () => {
+            let response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${department}`);
+            
+            if(!response.ok) {
+                setObjectIds(null);
+                console.error(response);
+                return;
+            }
 
-          let resJson = await response.json()
+            let resJson = await response.json()
 
-          console.log(resJson);
+            setObjectIds(resJson.objectIDs);
+        })();
+    }, [department]);
 
-          setObjectIds(resJson.objectIDs);
-      })();
-  }, [department]);
-
-  return (
-    <div className="App">
-      <QuestionPanel
-        objectIds={objectIds}
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <QuestionPanel
+              objectIds={objectIds}
+            />
+        </div>
+    );
 }
 
 export default App;
